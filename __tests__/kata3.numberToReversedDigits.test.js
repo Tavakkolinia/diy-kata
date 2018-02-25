da6c40343ca98700038086385ae8697c73b83538
gergana <gergana_ruse@yahoo.com>
2018-02-25 20:10:17 +0000

    tests

--- __tests__/kata3.numberToReversedDigits.test.js
+++ __tests__/kata3.numberToReversedDigits.test.js
@@ -2,6 +2,9 @@ import { numberToReversedDigits } from '../src'
 
 describe('numberToReversedDigits', () => {
   it('returns a reversed array of the number\'s digits', () => {
+    expect(numberToReversedDigits(4639)).toEqual([9, 3, 6, 4])
+    expect(numberToReversedDigits(79106)).toEqual([6, 0, 1, 9, 7])
+    expect(numberToReversedDigits(804510)).toEqual([0, 1, 5, 4, 0, 8])
     
   })
 })
--- __tests__/kata4.humanCatDogYears.test.js
+++ __tests__/kata4.humanCatDogYears.test.js
@@ -1,3 +1,9 @@
 import { humanCatDogYears } from '../src'
 
-// Look Ma, no handlebars!!!
+describe('humanCatDogYear', () => {
+  it('returns array of human, cat and dog years when passed human', () => {
+    expect(humanCatDogYears(10)).toEqual([10, 56, 64])
+    expect(humanCatDogYears(25)).toEqual([25, 116, 139])
+    expect(humanCatDogYears(13)).toEqual([13, 68, 79])
+  })
+})  
